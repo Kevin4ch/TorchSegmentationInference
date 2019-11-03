c8ab9c6ade79e8c917a97d7737ab8e3df90aaba9
Kevin4ch <kevin4ch@live.com>
2019-11-03 15:46:09 +0800

    Update TorchSegEngine.cpp

--- TorchSegEngine.cpp
+++ TorchSegEngine.cpp
@@ -1,5 +1,5 @@
 //
-// Created by kevin on 19-11-1.
+// Created by kevin4ch on 19-11-1.
 //
 
 #include "TorchSegEngine.h"
@@ -56,17 +56,21 @@ vector<SegObject> TorchSegEngine::segmentation(cv::Mat image) {
 
     at::Tensor result = torch::argmax(output_image[0], 0);
     cv::Mat mat(inputSize, CV_8UC1, result.data_ptr());
+    //模型正在训练，这个是标注。先用来测试后处理
     mat = cv::imread("/home/alien/CLionProjects/TorchSegmentation/test_result.png", -1);
     vector<SegObject> segObjects;
     for (int j = 1; j < cls_num; ++j) {
         cv::Mat cls_map;
         //to zero if px > threshold
+        // 将高于本类别的像素位置的值置零,其他值不变
         cv::threshold(mat.clone(), cls_map, j, 0, CV_THRESH_TOZERO_INV);
-        //to zero if px <= threshold
+        //to zero if px < threshold
+        //将高于本类(由于threshold函数只判大于因此使用 j - 1)的像素位置的值不变,其他值置零
         cv::threshold(cls_map, cls_map, j - 1, 0, CV_THRESH_TOZERO);
         //to 255 if px ==threshold
+        //将高于本类(j - 1)的像素值设置为255，其他值置零
         cv::threshold(cls_map, cls_map, j - 1, 255, CV_THRESH_BINARY);
-        //filter smaller cell px area
+        //腐蚀来清除部分噪声
         cv::erode(cls_map, cls_map, kernel);
         vector<vector<cv::Point>> contours;
         cv::findContours(cls_map, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
@@ -77,6 +81,7 @@ vector<SegObject> TorchSegEngine::segmentation(cv::Mat image) {
             rotatedRect.points(vertices);
             SegObject obj;
             for (int k = 0; k < 4; ++k) {
+                //模型输入的尺寸转回原图
                 int imagePosX = (int) ((vertices[k].x / (float) mat.cols) * imageWidth);
                 int imagePosy = (int) ((vertices[k].y / (float) mat.rows) * imageHeight);
                 obj.points.push_back(cv::Point(imagePosX, imagePosy));
