937c6edd37666444dac5a4b46b243f5d7d01741d
Kevin4ch <kevin4ch@live.com>
2019-11-03 15:47:17 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 int main() {
     TorchSegEngine engine("/home/alien/CLionProjects/TorchSegmentation/test.pt",cv::Size(512,512));
     cv::Mat mat = cv::imread("/home/alien/CLionProjects/TorchSegmentation/20190403_0000026513.jpg");
-
     vector<SegObject> obj = engine.segmentation(mat);
     engine.drawSegObjects(mat,obj,cv::Scalar(0,2,255),1);
     cv::imshow("",mat);
